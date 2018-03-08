5592a12e5bdf2aa28851f7c9b0ca5200a1bb1cc7
henrytasset <36865463+henrytasset@users.noreply.github.com>
2018-03-07 19:09:50 -0600

    Add files via upload

---
+++ bytes_homescreen_scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  var visible = 0;
+
+    $("#byteHomeButton").click(function(){
+      if(visible == 0){
+          $("#byteDropdown").show();
+      }
+      $visible = 1;
+    });
+
+    $("#byteHomeButton").click(function(){
+      if(visible == 1){
+          $("#byteDropdown").hide();
+      }
+      $visible = 0;
+    });
+
+});
