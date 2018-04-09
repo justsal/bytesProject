2e7cc8effdbe8235350c6afaff32ef27fbcabe07
Henry Tasset <henrytasset@gmail.com>
2018-04-09 14:32:58 -0500

    Stuff

--- scripts/questions.js
+++ scripts/questions.js
@@ -11,32 +11,18 @@ $(document).ready(function(){
 	$("section.htquestion2").hide();
 	$("span.back").hide();
 	$(".incorrectanimfade").hide();
-	
-	
+$(".correctanimfade").hide();
+
+
 $('.incorrect').click(function(){
 		$(".incorrectanimfade").show();
 		$("section.htquestion1").slideUp(1000);
 		$("section.htquestion2").slideUp(1000);
-		$(".incorrectanimfade").delay(3000).slideUp(1000);
 		
 	
 	
 		});
 	
-	$('.incorrect').click(function (e) {
-    e.preventDefault();                   // prevent default anchor behavior
-    var goTo = this.getAttribute("href"); // store anchor href
-
-    // do something while timeOut ticks ... 
-
-		$("section.htquestion1").slideUp(1000);
-		$("section.htquestion2").slideUp(1000);
-		$(".incorrectanimfade").delay(3000).slideUp(1000);
-		
-    setTimeout(function(){
-         window.location = goTo;
-    },3000);       
-}); 
 	
 
 //To stats from Nav Menu
